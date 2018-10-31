a6e26ebe147e7ebd0948d5e42820a1749235f833
Ahmad Qteifan <aqteifan@gmail.com>
2018-10-30 20:37:46 -0400

    Adding UI components and routes for them

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// the @ sign alies that point to root of our vue cli generated src folder
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import About from '@/components/About'
 
+// Use Router plugin
+// Give access to roter vue components
+// template markup will get injected in router-view tag in App.vue
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
